0423af1caaa23c12013db3603e24b1aa413c25c9
Bryan Mutell <bryanmmutell@gmail.com>
2025-03-10 21:13:21 -0600

    Update Setup.sql

--- Setup.sql
+++ Setup.sql
@@ -62,6 +62,10 @@ create schema fireworks;
 /**************************************************
 ******************** Corte Analyst Setup
 **************************************************/
+-- Download CSV from https://data.gilbertaz.gov/datasets/2dcb4c20c9a444f2a5d3dca180b651c9/explore
+-- compress file
+-- Upload to table calls_for_Service
+
 
 use role sysadmin;
 
